ac1a3fa0247afcaf221c5a534587c945fccd8904
Warre van der Maat <60901879+warrevdm@users.noreply.github.com>
2026-07-31 15:33:07 +0200

    Voeg gebruikersbeheer toe aan adminnavigatie

--- app/views.php
+++ app/views.php
@@ -25,6 +25,9 @@ function render_header(string $title, bool $showNav = true): void
                 <a href="index.php?route=planning">Planning</a>
                 <a href="index.php?route=reservation-new">Nieuwe verhuur</a>
                 <a href="bikes.php">Fietsen</a>
+                <?php if (($user['role'] ?? '') === 'admin'): ?>
+                    <a href="users.php">Gebruikers</a>
+                <?php endif; ?>
             </nav>
             <form method="post" action="index.php?route=logout" class="logout-form">
                 <input type="hidden" name="_token" value="<?= e(csrf_token()) ?>">
